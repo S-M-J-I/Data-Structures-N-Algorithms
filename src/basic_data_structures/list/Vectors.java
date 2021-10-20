4f5cb4eb31f33e9e4a87e0c362f4e2ccacb4f19b
S-M-J-I <jishanlion@gmail.com>
2021-10-20 13:55:59 +0600

    Refactored Vector to take custom objects

--- src/basic_data_structures/list/Vectors.java
+++ src/basic_data_structures/list/Vectors.java
@@ -1,19 +1,71 @@
 package basic_data_structures.list;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
+
+class CustomVector<T extends Object> {
+    List<T> list;
+    private int size;
+
+    public CustomVector() {
+        list = new ArrayList<>();
+    }
+
+    public void push_back(T item) {
+        list.add(item);
+        size++;
+    }
+
+    public void pop_back() {
+        list.remove(size-1);
+        size--;
+    }
+
+    public void push_front(T item) {
+        list.add(0, item);
+        size++;
+    }
+
+    public void pop_front() {
+        list.remove(0);
+        size--;
+    }
+
+    public void removeOccurrencesOf(T item) {
+        this.list.removeIf((i) -> i.equals(item));
+    }
+
+    public void show() {
+        for(T item : this.list) {
+            System.out.print(item+" ");
+        }
+        System.out.println();
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
 
 public class Vectors {
 
     public static void main(String[] args) {
-        List<Employee> employeeList = new Vector<>();
+        CustomVector<String> customVector = new CustomVector<>();
 
-        //Add to ArrayList object
-        employeeList.add(new Employee("Jack","Bauer",1838));
-        employeeList.add(new Employee("John","Doe",3543));
-        employeeList.add(new Employee("Frank","Ocean",6756));
-        employeeList.add(new Employee("Danny","Williams",5463));
+        customVector.push_front("a");
+        customVector.push_front("b");
+        customVector.push_front("c");
+        customVector.show();
+        customVector.pop_front();
+        customVector.pop_back();
+        customVector.show();
 
-        /*SAME AS ARRAYLIST*/
+        for(int i = 0; i < 10; ++i) {
+            customVector.push_back("c");
+        }
+        customVector.show();
+        customVector.removeOccurrencesOf("c");
+        customVector.show();
     }
 }
